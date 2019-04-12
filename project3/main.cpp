470f6176d5adc69f58699c7d0b8cd5bcc9178e63
David Klett <david.ger.klett@gmail.com>
2019-04-12 09:46:44 -0400

    edited main

--- project3/main.cpp
+++ project3/main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <stdio.h>      /* printf, fgets */
+#include <stdlib.h>
+
 #include "heapsort.h"
 #include "insertsort.h"
 #include "mergesort.h"
@@ -9,6 +15,36 @@ using namespace std;
 int main()
 {
     vector <int> a{2, 3, 5, 2, 3, 5, 2, 8, 9, 23, 55 , 43, 32};
+    string fileName;
+    cout << "Please enter the file name: ";
+    cin >> fileName;
 
+    string line;
+      ifstream myfile (fileName);
+      int countV = 0;
+
+      if (myfile.is_open())
+      {
+        //Iterate through vector:
+        while ( getline (myfile,line) )
+        {
+          //adjList.push_back(mtLst);
+
+                std::istringstream is( line );
+                //int x;
+                //populate the linked-list!
+               a[countV] = stoi(line);
+
+          countV++;
+        }
+        myfile.close();
+      }
+
+      else cout << "Unable to open file";
+
+
+    cout << a[1];
+    heapsort(a);
+    cout << a[1];
     return 0;
 }
